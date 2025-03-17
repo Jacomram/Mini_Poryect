28973b78b67a9f558f9ab6e50a053d4b4bb5a637
Jacobo <jacobojoseramirez@gmail.com>
2025-03-16 23:54:20 -0700

    Working on the home component

--- MiniProyect/src/app/complements/home/home.component.ts
+++ MiniProyect/src/app/complements/home/home.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatMenuModule,
+    MatListModule,
+    MatButtonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
+   ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class HomeComponent {
 
+export class AppComponent {
+  title = 'MiniProyect';
 }
